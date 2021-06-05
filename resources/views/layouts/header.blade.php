1e77504aa570e36d333584d1c14032972e5b433e
Andrew Arscott <aarscott@talkative.uk>
2021-06-05 16:06:11 +0100

    nicer search and mobile friendly

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,26 +1,26 @@
 <header>
-    <div id="header_container" class="">
-        <div id="rhino_header">
-            <img src="{{ asset('assets/rhino-logo.png') }}" alt="rhino logo" width="64px"/>
-            <div id="rhino_title">
-                Rhino Mobility
-            </div>
-        </div>
-        <nav>
-            <div id="nav_links">
-                <a href="/support">Support</a> <a href="/security">Security</a>
-            </div>
-            <div id="darktoggle">
-                <i class="fas fa-clouds-moon"></i>
-            </div>
-        </nav>
-    </div>
-    <div id="search_container">
-        <div class="rhino_search">
-            <form action="/search" method="get" style="float: right;">
-                <input type="text" placeholder="Search.." name="search">
-                <button type="submit" aria-label="Search"><i class="fa fa-search"></i></button>
-            </form>
-        </div>
-    </div>
+	<div id="header_container" class="">
+		<div id="rhino_header">
+			<img src="{{ asset('assets/rhino-logo.png') }}" alt="rhino logo" width="64px"/>
+			<div id="rhino_title">
+				Rhino Mobility
+			</div>
+		</div>
+		<nav>
+			<div id="nav_links">
+				<a href="/support">Support</a> <a href="/security">Security</a>
+			</div>
+			<div id="darktoggle">
+				<i class="fas fa-clouds-moon"></i>
+			</div>
+		</nav>
+	</div>
+	<div id="search_container">
+		<form action="/search" method="GET" class="quick-search">
+			<input type="text" placeholder="Search..." name="search" class="search-input">
+			<button class="search-button">
+				<i class="fas fa-search"></i>
+			</button>
+		</form>
+	</div>
 </header>
