cae40f4ba4f697583f65a395b91cd6a0ecd54a86
Andrew Arscott <aarscott@talkative.uk>
2021-06-09 23:16:38 +0100

    revert change

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
 </div>
 <div id="made_with_container">
   <div id="made_with">
-    &#169; Rhino Mobility, 2019-<?php echo date("Y"); ?>
+    &#169; Rhino Mobility, 2019-<?php echo date("Y"); ?>.
   </div>
 </div>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/tocbot/4.11.1/tocbot.min.js"></script>
