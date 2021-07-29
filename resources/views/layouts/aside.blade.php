667c0f765a5e1b9152acd5f22d91fd54a6eb2e57
Andrew Arscott <andrew@prometheuscomputing.co.uk>
2021-07-29 15:43:40 +0100

    some mobile menu workings

--- resources/views/layouts/aside.blade.php
+++ resources/views/layouts/aside.blade.php
@@ -1,5 +1,14 @@
 <div class="aside-container">
-    @includeWhen($content->type === 'doc', 'layouts.meta')
-    @includeWhen($content->type === 'doc', 'layouts.doc_nav')
-    @includeWhen($content->type === 'doc_parent' || $content->type === 'grid', 'layouts.page_nav')
+	@includeWhen($content->type === 'doc', 'layouts.meta')
+	@includeWhen($content->type === 'doc', 'layouts.doc_nav')
+	@includeWhen($content->type === 'doc_parent' || $content->type === 'grid', 'layouts.page_nav')
 </div>
+<div id="mobile_aside_contents">
+    <div class="aside-title" onclick="toggleTocMenu()">
+        <h2>Contents</h2>
+    </div>
+    <div id="mobile_article_contents_list" class="js-toc toc-content-hidden">
+        <!-- Taken care of by Tocbot -->
+    </div>
+</div>
+
