2147dcc80623c6adde2f0f03060acb95fb2f7b51
Andrew Arscott <andrew@prometheuscomputing.co.uk>
2021-09-02 09:51:02 +0100

    wip

--- app/Http/Controllers/AppDeploymentController.php
+++ app/Http/Controllers/AppDeploymentController.php
@@ -2,18 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\JsonResponse;
-use Symfony\Component\Process\Process;
-use Illuminate\Support\Facades\Artisan;
-
 class AppDeploymentController extends Controller {
 
 	public function __invoke() {
 //		Artisan::call('deploy');
+		echo 'the script is' . base_path() . '/deploy.sh';
 		$command = new \Symfony\Component\Process\Process(['sh', base_path() . '/deploy.sh']);
 		$command->setTimeout(15);
 		$command->run(function ($type, $buffer) {
-			echo $buffer;
+			echo $buffer . '\r\n';
 		});
 	}
 }
