a34d01c5838ce46b41c4e22d6f4a2757725ee6ee
Andrew Arscott <andrew@prometheuscomputing.co.uk>
2021-09-02 10:08:43 +0100

    wip

--- app/Console/Commands/AppDeployment.php
+++ app/Console/Commands/AppDeployment.php
@@ -18,20 +18,127 @@ class AppDeployment extends Command {
 	 *
 	 * @var string
 	 */
-	protected $description = 'Pulls and builds the application';
+	protected $description = 'Pull files from GIT';
+
+	/**
+	 * Is the code already updated or not
+	 *
+	 * @var boolean
+	 */
+	private $alreadyUpToDate;
+
+	/**
+	 * Log from git pull
+	 *
+	 * @var array
+	 */
+	private $pullLog = [];
+
+	/**
+	 * Log from composer install
+	 *
+	 * @var boolean
+	 */
+	private $composerLog = [];
+
+
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		parent::__construct();
+	}
+
 
 	/**
 	 * Execute the console command.
 	 *
-	 * @return int
+	 * @return mixed
 	 */
 	public function handle() {
-		$command = new Process(['sh', base_path() . '/deploy.sh']);
-		$command->setTimeout(15);
-		$command->run(function ($type, $buffer) {
-			$this->info($buffer);
+
+		if (!$this->runPull()) {
+
+			$this->error("An error occurred while executing 'git pull'. \nLogs:");
+
+			foreach ($this->pullLog as $logLine) {
+				$this->info($logLine);
+			}
+
+			return;
+		}
+
+
+		if ($this->alreadyUpToDate) {
+			$this->info("The application is already up-to-date");
+			return;
+		}
+
+
+		if (!$this->runComposer()) {
+
+			$this->error("Error while updating composer files. \nLogs:");
+
+			foreach ($this->composerLog as $logLine) {
+				$this->info($logLine);
+			}
+
+			return;
+		}
+
+		$this->info("Succesfully updated the application.");
+
+
+	}
+
+
+	/**
+	 * Run git pull process
+	 *
+	 * @return boolean
+	 */
+
+	private function runPull() {
+
+		$process = new Process(['git', 'pull']);
+		$this->info("Running 'git pull'");
+
+		$process->run(function ($type, $buffer) {
+			$this->pullLog[] = $buffer;
+
+			if ($buffer == "Already up to date.\n") {
+				$this->alreadyUpToDate = true;
+			}
+
 		});
 
-		return 0;
+		return $process->isSuccessful();
+
 	}
+
+
+	/**
+	 * Run composer install process
+	 *
+	 * @return boolean
+	 */
+
+	private function runComposer() {
+
+		$process = new Process(['composer', 'install']);
+		$this->info("Running 'composer install'");
+
+		$process->run(function ($type, $buffer) {
+			$this->composerLog[] = $buffer;
+		});
+
+
+		return $process->isSuccessful();
+
+
+	}
+
+
 }
