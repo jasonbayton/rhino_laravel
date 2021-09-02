5dc5ac3af05412e4454145cdfd7447c1528895bb
Andrew Arscott <andrew@prometheuscomputing.co.uk>
2021-09-02 09:12:58 +0100

    test updating the deployment controller

--- app/Http/Controllers/AppDeploymentController.php
+++ app/Http/Controllers/AppDeploymentController.php
@@ -8,10 +8,18 @@ use Illuminate\Support\Facades\Artisan;
 class AppDeploymentController extends Controller {
 
 	public function __invoke(): JsonResponse {
-		Artisan::call('deploy');
+//		Artisan::call('deploy');
+		$logging = '';
+		$command = new Process(['sh', base_path() . '/deploy.sh']);
+		$command->setTimeout(15);
+		$command->run(function ($type, $buffer) use ($logging) {
+			$logging .= $buffer;
+		});
 
 		return response()->json([
 			'status' => 'success',
+			'logging' => $logging,
 		]);
+
 	}
 }
