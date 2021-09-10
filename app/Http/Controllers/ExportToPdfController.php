fe3b264c180abc5593ea106692be48095d52e5bf
Andrew Arscott <andrew@prometheuscomputing.co.uk>
2021-09-10 11:33:58 +0100

    improve pdf download

--- app/Http/Controllers/ExportToPdfController.php
+++ app/Http/Controllers/ExportToPdfController.php
@@ -11,7 +11,19 @@ class ExportToPdfController extends Controller {
 		$content = $contentService->getByUrl($content);
 
 		$pdf = App::make('dompdf.wrapper');
-		$pdf->loadHTML($content->content());
+		$html = '';
+
+		if ($content->title) {
+			$html .= '<h1>' . $content->title . '</h1>';
+		}
+
+		if ($content->subtitle) {
+			$html .= '<h2>' . $content->subtitle . '</h2>';
+		}
+
+		$html .= $content->content();
+
+		$pdf->loadHTML($html);
 		return $pdf->download($content->title . '.pdf');
 	}
 }
