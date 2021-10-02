70fac6f2cdc291cafadbe5208ef094c15e4175b4
Jason Bayton <jason@bayton.org>
2021-10-02 21:23:18 +0100

    ?

--- resources/views/layouts/doc_nav.blade.php
+++ resources/views/layouts/doc_nav.blade.php
@@ -1,6 +1,7 @@
 <div id="aside_docs">
 	<div class="aside-title" onclick="toggleNavMenu()">
-		<h2>{{ $content->parentID ?? 'Navigation' }}</h2>
+		<!--h2>{{ $content->parentID ?? 'Navigation' }}</h2-->
+		<h2>Support</h2>
 	</div>
 	<div id="aside-menu" class="aside-content-hidden">
 		@foreach($topics as $header => $entries)
