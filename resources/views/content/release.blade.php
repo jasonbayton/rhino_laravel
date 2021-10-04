6298d9dd706eee6791fcf0efc51328be80bb066f
Jason Bayton <jason@bayton.org>
2021-10-04 18:31:27 +0100

    commit release doctype

---
+++ resources/views/content/release.blade.php
@@ -0,0 +1,79 @@
+@extends('layouts.app')
+
+@section('content')
+	<section id="content_container">
+		<aside class="left">
+			@include('layouts.aside')
+		</aside>
+		<article>
+			@if ($content->topic && $content->parent)
+				@include('layouts.breadcrumb')
+			@endif
+			<h2 id="article_title">
+				{{ $content->title }}
+			</h2>
+			@if($content->subtitle)
+				<div id="article_subtitle">{{ $content->subtitle }}</div>
+			@endif
+			@if($content->content())
+				<div id="literal_content" class="js-toc-content">
+					{!! $content->content() !!}
+				</div>
+
+			<h2>Releases</h2>
+			<div id="releases_list">
+				<table class="tg" id="support_table">
+					<thead>
+					  <tr>
+					    <th class="tg-0lax">
+								SKU
+							</th>
+					    <th class="tg-0lax">
+								TYPE
+							</th>
+					    <th class="tg-0lax">
+								Build #
+							</th>
+					    <th class="tg-0lax">
+								SPL
+							</th>
+					  </tr>
+					</thead>
+					<tbody>
+					  <tr>
+					    <td class="tg-0lax">
+								{{ $result->softwaresku }}
+							</td>
+					    <td class="tg-0lax">
+								{{ $result->releasetype }}
+							</td>
+					    <td class="tg-0lax">
+								{{ $result->title }}
+							</td>
+					    <td class="tg-0lax">
+								{{ $result->softwarespl }}
+							</td>
+					  </tr>
+					</tbody>
+				</table>
+
+				<ul>
+					@foreach($content->getChildren() as $result)
+						<li><a href="{{ $result->url }}">{{ $result->title }}</a></li>
+					@endforeach
+				</ul>
+			</div>
+			@endif
+			<div class="article-bottom-links">
+				<a target="_blank" href="{{ route('export-to-pdf', ['content' => $content->url]) }}"><i
+							style="padding-right: 10px;" class="fas fa-file"></i>Download as PDF</a>
+				<a target="_blank" href=""><i style="padding-right: 10px;" class="far fa-exclamation-triangle"></i>Report
+					Content</a>
+				<a target="_blank" onclick="window.print()"><i style="padding-right: 10px;" class="fas fa-print"></i>Print</a>
+			</div>
+			@if($content->mailingSignup())
+				{!! $content->mailingSignup() !!}
+			@endif
+		</article>
+	</section>
+@endsection
