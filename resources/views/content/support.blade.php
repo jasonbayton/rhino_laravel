d7448c6b8dc4a524b450d97cde9704297c2306a8
Jason Bayton <jason@bayton.org>
2021-10-13 14:00:38 +0100

    remove the crap

--- resources/views/content/support.blade.php
+++ resources/views/content/support.blade.php
@@ -6,11 +6,11 @@
             @include('layouts.aside')
         </aside>
         <article>
-            <h2 id="article_title">
-                {{ $content->title }}
-            </h2>
-            <!-- foreach topic, output docs in a UL -->
-            <div class="content-grid">
+          <h2 id="article_title">
+            {{ $content->title }}
+          </h2>
+          <!-- foreach topic, output docs in a UL -->
+          <div class="content-grid">
             @foreach($allTopics as $header => $topic)
             <div class="grid-topic">
                 <div class="grid-topic-title">
@@ -24,22 +24,6 @@
               </div>
             @endforeach
           </div>
-            @foreach(array_chunk($content->getChildren()->toArray(), 3) as $chunk)
-                <div class="content-grid">
-                    @foreach($chunk as $entry)
-                        <div class="grid-topic">
-                            <div class="grid-topic-title">
-                                <h4>{{ $entry->title }} ({{ count($entry->getChildren()) }})</h4>
-                            </div>
-                            <ul>
-                                @foreach($entry->getChildren() as $child)
-                                    <li><a href="{{ $child->url }}">{{ $child->title }}</a></li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endforeach
-                </div>
-            @endforeach
         </article>
     </section>
 
