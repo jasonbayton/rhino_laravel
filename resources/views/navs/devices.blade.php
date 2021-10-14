1b1ac140a4e4821e241fb4308c9376c8abee5834
Jason Bayton <jason@bayton.org>
2021-10-14 13:57:08 +0100

    global test

--- resources/views/navs/devices.blade.php
+++ resources/views/navs/devices.blade.php
@@ -1,8 +1,12 @@
-<div id="aside_page">
-	<div class="aside-title" onclick="toggleNavMenu()">
-		<h2>Navigation</h2>
-	</div>
-	<div id="aside-menu" class="aside-content-hidden docnav-topic">
+<div class="aside-title" onclick="toggleNavMenu()">
+  <h2>Navigation</h2>
+</div>
+<div id="aside-menu" class="aside-content-hidden docnav-topic">
+  <details>
+    <summary class="docnav-topic-heading">
+      Devices
+    </summary>
+    <!-- list all pages with parent Devices here -->
     <ul>
     @foreach($devicesmenu as $header => $entry)
       <li>
@@ -18,6 +22,49 @@
         @endif
       </li>
     @endforeach
-  </ul>
-  </div>
-</div>
+    </ul>
+  </details>
+
+  <details>
+    <summary class="docnav-topic-heading">
+      Support
+    </summary>
+
+    @foreach($menu as $header => $entries)
+			<details @if($content->topic !== '' && strtolower($header) == strtolower($content->topic)) open @endif  class="docnav-topic">
+				<summary class="docnav-topic-title">
+					{{ $header }}
+				</summary>
+				<ul>
+					@foreach($entries as $entry)
+						<li><a href="{{ $entry->url }}">{{ $entry->title }}</a></li>
+					@endforeach
+				</ul>
+			</details>
+		@endforeach
+
+  </details>
+
+  <details>
+    <summary class="docnav-topic-heading">
+      Security
+    </summary>
+    <!-- list all pages with parent Security here -->
+    <ul>
+    @foreach($devicesmenu as $header => $entry)
+      <li>
+        <a href="{{ $entry->url }}">{{ $entry->title }}</a>
+        @if($entry->getChildren()->isNotEmpty())
+          <ul>
+            @foreach($entry->getChildren() as $child)
+              <li>
+                <a href="{{ $child->url }}">{{ $child->title }}</a>
+              </li>
+            @endforeach
+          </ul>
+        @endif
+      </li>
+    @endforeach
+    </ul>
+
+  </details>
