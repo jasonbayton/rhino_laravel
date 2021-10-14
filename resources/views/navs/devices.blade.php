016a3cca6f4e046dc3c5fc8e57e86678d54f371e
Jason Bayton <jason@bayton.org>
2021-10-14 16:10:59 +0100

    add in placeholders for docnav

--- resources/views/navs/devices.blade.php
+++ resources/views/navs/devices.blade.php
@@ -3,6 +3,10 @@
 		<h2>Navigation</h2>
 	</div>
 	<div id="aside-menu" class="aside-content-hidden docnav-topic">
+    <details>
+      <summary class="docnav-topic-heading">
+        Devices
+      </summary>
     <ul>
     @foreach($devicesmenu as $header => $entry)
       <li>
@@ -19,6 +23,19 @@
       </li>
     @endforeach
   </ul>
+</details>
+
+<details>
+  <summary class="docnav-topic-heading">
+    Support
+  </summary>
+</details>
+
+<details>
+  <summary class="docnav-topic-heading">
+    Security
+  </summary>
+</details>
   </div>
 
 </div>
