b87e22698c01485e8d4de1e6fe0d2ae8b3bcf9aa
Jason Bayton <jason@bayton.org>
2021-10-14 14:05:27 +0100

    support

--- resources/views/navs/devices.blade.php
+++ resources/views/navs/devices.blade.php
@@ -20,4 +20,24 @@
     @endforeach
   </ul>
   </div>
+
+  <details>
+    <summary class="docnav-topic-heading">
+      Support
+    </summary>
+
+    @foreach($menu as $header => $entries)
+			<details @if($content->topic !== '' && strtolower($header) == strtolower($content->topic)) open @endif  class="docnav-topic">
+				<summary class="docnav-topic-title">
+					{{ $header }}
+				</summary>
+				<ul>
+					@foreach($entries as $entry)
+						<li><a href="{{ $entry->url }}">{{ $entry->title }}</a></li>
+					@endforeach
+				</ul>
+			</details>
+		@endforeach
+
+  </details>
 </div>
