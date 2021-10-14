0124a485d23d8b061ba5f50c056dd8d641c9b729
Jason Bayton <jason@bayton.org>
2021-10-14 14:03:58 +0100

    revert

--- resources/views/navs/devices.blade.php
+++ resources/views/navs/devices.blade.php
@@ -1,12 +1,8 @@
-<div class="aside-title" onclick="toggleNavMenu()">
-  <h2>Navigation</h2>
-</div>
-<div id="aside-menu" class="aside-content-hidden docnav-topic">
-  <details>
-    <summary class="docnav-topic-heading">
-      Devices
-    </summary>
-    <!-- list all pages with parent Devices here -->
+<div id="aside_page">
+	<div class="aside-title" onclick="toggleNavMenu()">
+		<h2>Navigation</h2>
+	</div>
+	<div id="aside-menu" class="aside-content-hidden docnav-topic">
     <ul>
     @foreach($devicesmenu as $header => $entry)
       <li>
@@ -22,33 +18,6 @@
         @endif
       </li>
     @endforeach
-    </ul>
-  </details>
-
-  <details>
-    <summary class="docnav-topic-heading">
-      Support
-    </summary>
-
-    @foreach($menu as $header => $entries)
-			<details @if($content->topic !== '' && strtolower($header) == strtolower($content->topic)) open @endif  class="docnav-topic">
-				<summary class="docnav-topic-title">
-					{{ $header }}
-				</summary>
-				<ul>
-					@foreach($entries as $entry)
-						<li><a href="{{ $entry->url }}">{{ $entry->title }}</a></li>
-					@endforeach
-				</ul>
-			</details>
-		@endforeach
-
-  </details>
-
-  <details>
-    <summary class="docnav-topic-heading">
-      Security
-    </summary>
-    <!-- list all pages with parent Security here -->
-
-  </details>
+  </ul>
+  </div>
+</div>
