82e1bb56e5da6ab80c3c91fe125d0ae01695c0e4
Jason Bayton <jason@bayton.org>
2021-10-14 09:29:56 +0100

    adjust layout

--- resources/views/content/release.blade.php
+++ resources/views/content/release.blade.php
@@ -29,85 +29,85 @@
 					the time taken for the update to become available.</p>
 				<p>The releases below are in date order (newest > oldest). For release information, please click the build number of a release, or to
 					directly download the update (either incremental or full) click the respective download icon.</p>
-			</div>
-			<h2>Releases</h2>
-			<div id="releases_list">
-				<div id="support_table">
-				<table class="tg">
-					<thead>
-					  <tr>
-					    <th class="tg-0lax">
-								SKU
-							</th>
-					    <th class="tg-0lax">
-								Type
-							</th>
-					    <th class="tg-0lax">
-								Build #
-							</th>
-					    <th class="tg-0lax">
-								SPL
-							</th>
-					    <th class="tg-0lax">
-								OTA
-							</th>
-							</th>
-					    <th class="tg-0lax">
-								Full
-							</th>
-					  </tr>
-					</thead>
-					<tbody>
-						@foreach($content->getChildren() as $result)
-					  <tr>
-					    <td class="tg-0lax">
-								@isset($result->softwaresku)
-									{{ $result->softwaresku }}
-								@endisset
-							</td>
-					    <td class="tg-0lax">
-								@isset($result->releasetype)
-									{{ $result->releasetype }}
-								@endisset
-							</td>
-					    <td class="tg-0lax">
-								@isset($result->title)
-									<a href="{{ $result->url }}">{{ $result->title }}</a>
-								@endisset
-							</td>
-					    <td class="tg-0lax">
-								@isset($result->softwarespl)
-									{{ $result->softwarespl }}
-								@endisset
-							</td>
-					    <td class="tg-0lax">
-								@isset($result->otapackageurl)
-								@if($result->otapackageurl !== '')
-									<a href="{{ $result->otapackageurl }}"><i class="far fa-cloud-download-alt"></i></a>
-								@endif
-								@endisset
-							</td>
-							</td>
-					    <td class="tg-0lax">
-								@isset($result->fullotaurl)
-								@if($result->fullotaurl !== '')
-									<a href="{{ $result->fullotaurl }}"><i class="fas fa-cloud-download-alt"></i></a>
-								@endif
-								@endisset
-							</td>
-					  </tr>
-						@endforeach
-					</tbody>
-				</table>
-				<p>
-					<b>SKU</b> - The unique software branch. Global is 001, for reference. <br>
-					<b>Type</b> - The type of release: maintenance (MR), security (SMR), launch (LR), or initial (IR). <br>
-					<b>Build #</b> - The build number of the software release. <br>
-					<b>SPL</b> - The Security Patch Level applied, these are published by Google monthly, and applied to Rhino devices on a 90 day basis. <br>
-					<b>OTA</b> - The OTA file containing the standard, incremental update normally sent to devices. Use <a href="/support/update-via-adb">ADB sideload</a> or <a href="/support/update-via-sdcard">SD upgrade</a> to update. <br>
-					<b>Full</b> - The OTA file containing the <i>full</i> update package which can be applied over any <b>previous</b> software version to bring the device up to date immediately, avoiding multiple incremental updates. Use <a href="/support/update-via-adb">ADB sideload</a> or <a href="/support/update-via-sdcard">SD upgrade</a> to update.
-				</p>
-			</div>
+				<h2>Releases</h2>
+				<div id="releases_list">
+					<div id="support_table">
+						<table class="tg">
+							<thead>
+							  <tr>
+							    <th class="tg-0lax">
+										SKU
+									</th>
+							    <th class="tg-0lax">
+										Type
+									</th>
+							    <th class="tg-0lax">
+										Build #
+									</th>
+							    <th class="tg-0lax">
+										SPL
+									</th>
+							    <th class="tg-0lax">
+										OTA
+									</th>
+									</th>
+							    <th class="tg-0lax">
+										Full
+									</th>
+							  </tr>
+							</thead>
+							<tbody>
+								@foreach($content->getChildren() as $result)
+							  <tr>
+							    <td class="tg-0lax">
+										@isset($result->softwaresku)
+											{{ $result->softwaresku }}
+										@endisset
+									</td>
+							    <td class="tg-0lax">
+										@isset($result->releasetype)
+											{{ $result->releasetype }}
+										@endisset
+									</td>
+							    <td class="tg-0lax">
+										@isset($result->title)
+											<a href="{{ $result->url }}">{{ $result->title }}</a>
+										@endisset
+									</td>
+							    <td class="tg-0lax">
+										@isset($result->softwarespl)
+											{{ $result->softwarespl }}
+										@endisset
+									</td>
+							    <td class="tg-0lax">
+										@isset($result->otapackageurl)
+										@if($result->otapackageurl !== '')
+											<a href="{{ $result->otapackageurl }}"><i class="far fa-cloud-download-alt"></i></a>
+										@endif
+										@endisset
+									</td>
+									</td>
+							    <td class="tg-0lax">
+										@isset($result->fullotaurl)
+										@if($result->fullotaurl !== '')
+											<a href="{{ $result->fullotaurl }}"><i class="fas fa-cloud-download-alt"></i></a>
+										@endif
+										@endisset
+									</td>
+							  </tr>
+								@endforeach
+							</tbody>
+						</table>
+						<p>
+							<b>SKU</b> - The unique software branch. Global is 001, for reference. <br>
+							<b>Type</b> - The type of release: maintenance (MR), security (SMR), launch (LR), or initial (IR). <br>
+							<b>Build #</b> - The build number of the software release. <br>
+							<b>SPL</b> - The Security Patch Level applied, these are published by Google monthly, and applied to Rhino devices on a 90 day basis. <br>
+							<b>OTA</b> - The OTA file containing the standard, incremental update normally sent to devices. Use <a href="/support/update-via-adb">ADB sideload</a> or <a href="/support/update-via-sdcard">SD upgrade</a> to update. <br>
+							<b>Full</b> - The OTA file containing the <i>full</i> update package which can be applied over any <b>previous</b> software version to bring the device up to date immediately, avoiding multiple incremental updates. Use <a href="/support/update-via-adb">ADB sideload</a> or <a href="/support/update-via-sdcard">SD upgrade</a> to update.
+						</p>
+					</div>
+				</div>
 			</div>
 			<div class="article-bottom-links">
 				<a target="_blank" href="{{ route('export-to-pdf', ['content' => $content->url]) }}"><i
