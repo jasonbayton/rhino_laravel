dc2704a71fd897e8add87fea55e1410fbfb54d4b
Jason Bayton <jason@bayton.org>
2021-10-14 12:59:36 +0100

    put a bit of content there for a moment

--- resources/views/navs/devices.blade.php
+++ resources/views/navs/devices.blade.php
@@ -1,1 +1,7 @@
-
+<div id="aside_page">
+	<div class="aside-title" onclick="toggleNavMenu()">
+		<h2>Navigation</h2>
+	</div>
+	<div id="aside-menu" class="aside-content-hidden docnav-topic">
+  </div>
+</div>
