d1fac00b53e1970b06ea1150e4ef4df7d1b7c14c
Jason Bayton <jason@bayton.org>
2021-10-14 12:35:42 +0100

    remove redundant stuff

--- resources/views/layouts/aside.blade.php
+++ resources/views/layouts/aside.blade.php
@@ -4,12 +4,11 @@
 {{--	@includeWhen($content->type === 'doc', 'layouts.doc_nav')--}}
 {{--	@includeWhen($content->type === 'doc_parent' || $content->type === 'grid', 'layouts.page_nav')--}}
 </div>
-<div id="mobile_aside_contents">
+<!--div id="mobile_aside_contents">
     <div class="aside-title" onclick="toggleTocMenu()">
         <h2>Contents</h2>
     </div>
     <div id="mobile_article_contents_list" class="js-toc-mobile toc-content-hidden">
         <!-- Taken care of by Tocbot -->
-    </div>
-</div>
-
+    <!--/div-->
+<!--/div-->
