6148cfce1ab18a5695def5a3560ec527d47d1098
Jason Bayton <jason@bayton.org>
2021-10-14 13:04:43 +0100

    back again

--- resources/views/navs/devices.blade.php
+++ resources/views/navs/devices.blade.php
@@ -3,5 +3,21 @@
 		<h2>Navigation</h2>
 	</div>
 	<div id="aside-menu" class="aside-content-hidden docnav-topic">
+    <ul>
+    @foreach($menu as $header => $entry)
+      <li>
+        <a href="{{ $entry->url }}">{{ $entry->title }}</a>
+        @if($entry->getChildren()->isNotEmpty())
+          <ul>
+            @foreach($entry->getChildren() as $child)
+              <li>
+                <a href="{{ $child->url }}">{{ $child->title }}</a>
+              </li>
+            @endforeach
+          </ul>
+        @endif
+      </li>
+    @endforeach
+  </ul>
   </div>
 </div>
