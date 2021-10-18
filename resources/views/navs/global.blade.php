06b48641a8189a765a68b19602a5ad6a386b8fa1
Jason Bayton <jason@bayton.org>
2021-10-18 17:57:12 +0100

    test1

--- resources/views/navs/global.blade.php
+++ resources/views/navs/global.blade.php
@@ -47,30 +47,7 @@
 </div>
   </details>
 </div>
-<div class="globnav-wrapper">
-  <details @if($content->parent !== '' && strtolower($content->parent) == "security") open @endif  class="docnav-topic">
-    <summary class="docnav-topic-heading">
-      Security
-    </summary>
-    <!-- list all pages with parent Security here -->
-    <ul>
-    @foreach($securitymenu as $header => $entry)
-      <li>
-        <a href="{{ $entry->url }}">{{ $entry->title }}</a>
-        @if($entry->getChildren()->isNotEmpty())
-          <ul>
-            @foreach($entry->getChildren() as $child)
-              <li>
-                <a href="{{ $child->url }}">{{ $child->title }}</a>
-              </li>
-            @endforeach
-          </ul>
-        @endif
-      </li>
-    @endforeach
-    </ul>
-  </details>
-</div>
+
 
 <div class="globnav-wrapper">
   <details @if($content->parent !== '' && strtolower($content->parent) == "security") open @endif  class="docnav-topic">
