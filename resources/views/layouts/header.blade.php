76a5b4fd5af410d84b17ae322da9244af8dccdbf
Jason Bayton <jason@bayton.org>
2021-11-17 10:23:30 +0000

    add burger to front, css fix to come

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -7,6 +7,45 @@
 					RHINO
 				</div>
 			</a>
+			<div class="burger" onclick="toggleMainMenu(this)">
+				<div class="bar1"></div>
+				<div class="bar2"></div>
+				<div class="bar3"></div>
+			</div>
+		</div>
+		<div id="menu_overlay" class="menu_overlay_hidden">
+			<div id="menu_header">
+				<h2 class="menu_overlay_title">
+					Menu
+				</h2>
+				<div id="darktoggle">
+					<i class="fas fa-clouds-moon"></i>
+				</div>
+			</div>
+			<div class="menu_overlay_content">
+				@foreach($mainMenu as $menu)
+					@if(isset($menu['href']))
+					<div class="docnav-topic">
+						<div class="docnav-topic-title">
+							<a href="{{ $menu['href'] }}">{{ $menu['name'] }}</a>
+						</div>
+					</div>
+					@else
+					<details class="docnav-topic">
+						<summary class="docnav-topic-title">
+							{{ $menu['name'] }}
+						</summary>
+						<ul>
+							@if(isset($menu['items']))
+								@foreach($menu['items'] as $subMenuItem)
+									<li><a href="{{ $subMenuItem['href'] }}">{{ $subMenuItem['name'] }}</a></li>
+								@endforeach
+							@endif
+						</ul>
+					</details>
+					@endif
+				@endforeach
+			</div>
 		</div>
 	@else
 		<div id="header_container" class="">
@@ -16,14 +55,6 @@
 					RHINO
 				</div>
 			</a>
-			<!--nav>
-				<div id="nav_links">
-					<a href="/support">Support</a> <a href="/security">Security</a>
-				</div>
-				<div id="darktoggle">
-					<i class="fas fa-clouds-moon"></i>
-				</div>
-			</nav-->
 			<div id="search_container_nav">
 				<form action="/search" method="GET" class="quick-search nav-quick-search">
 					<input type="text" placeholder="Search..." name="search" class="search-input nav-search-input">
